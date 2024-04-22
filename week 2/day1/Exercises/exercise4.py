a40132638fb6bce9c34c2c578f835315de5f523d
jessandria <jessie.andriamanankasina@gmail.com>
2024-04-22 18:18:16 +0400

    exercise2;3;4;5

---
+++ week 2/day1/Exercises/exercise4.py
@@ -0,0 +1,2 @@
+computer_brand = "MAcBook"
+print(f"I have a {computer_brand} computer")
---
+++ week 2/day1/Exercises/exercise5.py
@@ -0,0 +1,5 @@
+name="Jess"
+age="18"
+shoe_size="36"
+
+print(f"Hello! My name is {name}. Today is my birthday, i just turned {age}. My family gave me as a gift nike shoes, unfortunately they took the wrong size. THey didn't know my shoe size is {shoe_size}")

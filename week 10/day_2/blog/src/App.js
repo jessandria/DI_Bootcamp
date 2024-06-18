3250b05a3c44012fb0d2df680b4c3f2fcbaedae6
jessandria <jessie.andriamanankasina@gmail.com>
2024-06-18 10:59:53 +0400

    app

--- week 10/day_2/blog/src/App.js
+++ week 10/day_2/blog/src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+const charactersJson = {
+  "people": [
+    {
+      "id" : "1",
+      "name": "Luke Skywalker",
+      "height": "172",
+      "mass": "77",
+      "hair_color": "blond",
+    },
+    {
+      "id" : "2",
+      "name": "C-3PO",
+      "height": "167",
+      "mass": "75",
+      "hair_color": "n/a",
+    },
+    {
+      "id" : "3",
+      "name": "R2-D2",
+      "height": "96",
+      "mass": "32",
+      "hair_color": "n/a",
+    }
+  ]
+}
+
+const characters = JSON.parse(JSON.stringify(charactersJson));
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='people'>
+      <h1>In the App.js</h1>
+      {
+        characters.people.map(item => (
+          <ul>
+            <li>{item.name}</li>
+            <li>{item.height}</li>
+            <li>{item.hair_color}</li>
+          </ul>
+        ))
+      }
     </div>
   );
 }
